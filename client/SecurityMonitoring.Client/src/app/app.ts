aabe62a728a008803f926041fcde056cac652f31
Danial Hassan <danial.github@gmail.com>
2026-09-15 18:51:49 +0500

    Implement sidebar and topbar

--- client/SecurityMonitoring.Client/src/app/app.ts
+++ client/SecurityMonitoring.Client/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Dashboard } from './features/dashboard/dashboard';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Topbar } from './layout/topbar/topbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard],
+  imports: [RouterOutlet, Dashboard, Sidebar, Topbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- client/SecurityMonitoring.Client/src/app/features/dashboard/dashboard.ts
+++ client/SecurityMonitoring.Client/src/app/features/dashboard/dashboard.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
