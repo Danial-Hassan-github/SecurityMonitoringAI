5a3b6738c3255f0806f2da46a3d7d675c0de9d06
Danial Hassan <danial.github@gmail.com>
2026-09-19 19:53:51 +0500

    set up PostgreSQL database

--- api/SecurityMonitoringAI.API/SecurityMonitoringAI.API/Program.cs
+++ api/SecurityMonitoringAI.API/SecurityMonitoringAI.API/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SecurityMonitoring.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<SecurityMonitoringDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
